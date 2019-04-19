71b55f49e62b8fe1a60dc5a82824f10c06bdd640
Tomhsu1 <tom268@gmail.com>
2019-04-18 17:53:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ function drop(ev) {
   var data = ev.dataTransfer.getData("text");
   ev.target.appendChild(document.getElementById(data));
 }
+
+var x = 10;
+
+function movingElement() {
+  elmt = document.getElementById("fuck");
+  if (x < 200) {
+    x = x + 1;
+  } else {
+    x = 10;
+  }
+  elmt.style.marginLeft = x + "px";
+  
+}
+
+window.setInterval(movingElement, 10);
